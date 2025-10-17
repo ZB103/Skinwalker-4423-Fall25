86c0c9cf950a79cc023abc868369067f7ba727ac
Ani B <zbaker985@gmail.com>
2025-10-17 16:52:18 -0500

    complete algorithm minus DST

--- proj5_timelock/timelock.go
+++ proj5_timelock/timelock.go
@@ -5,33 +5,59 @@ import (
 	"os"
 	"bufio"
 	"time"
-	"strconv"
 	"strings"
 	"crypto/md5"
 	"encoding/hex"
+	"unicode"
 )
 
 //function that takes in user-provided epoch and sys time
 //and performs timelock, then returns derived code
 func runTimelock(epoch time.Time, sysTime time.Time) string{
-	var code int
-	
 	//get time elapsed since epoch
 	timeDifference := sysTime.Sub(epoch)
+	//deal with potential daylight savings time
+	if sysTime.IsDST() {
+		fmt.Println("sysTime is in DST")
+	}
 	
-	//encode using md5
+	//double encode using md5
 	hash := md5.Sum([]byte(timeDifference.String()))
 	hashString := hex.EncodeToString(hash[:])
-	fmt.Println(hashString)
+	dblHash := md5.Sum([]byte(hashString))
+	dblHashString := hex.EncodeToString(dblHash[:])
 	
 	//extract and concat first two letters of hash LtoR
+	var firstLetters string
+	for i := range len(dblHashString){
+		//is it a letter?
+		if unicode.IsLetter(rune(dblHashString[i])){
+			firstLetters = firstLetters + string(dblHashString[i])
+		}
+		//did we get two letters?
+		if len(firstLetters) == 2{
+			break
+		}
+	}
 	
 	//extract and concat first two single-digit ints of hash LtoR
+	var firstNumbers string
+	for i := len(dblHashString)-1; i >= 0; i--{
+		//is it a letter?
+		if unicode.IsNumber(rune(dblHashString[i])){
+			firstNumbers = firstNumbers + string(dblHashString[i])
+		}
+		//did we get two letters?
+		if len(firstNumbers) == 2{
+			break
+		}
+	}
 	
 	//concat two extracted values
+	code := firstLetters + firstNumbers
 	
 	//return code as a string
-	return strconv.Itoa(code)
+	return code
 }
 
 func main(){
@@ -52,11 +78,11 @@ func main(){
 	}
 	
 	//get system time
-	sysTime := time.Now().Round(time.Second)
+	sysTime := time.Now().Round(time.Second).UTC()
 	fmt.Println(epoch)
 	fmt.Println(sysTime)
 	
 	//calculate 4-character code
 	code := runTimelock(epoch, sysTime)
-	fmt.Println("Your code is: ", code)
+	fmt.Println(code)
 }
